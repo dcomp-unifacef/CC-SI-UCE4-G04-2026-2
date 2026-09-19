d8e2982372e12aea8f7d85f2c0124a76d2577b3b
rosa7788 <rosa05meneghetti@gmail.com>
2026-09-19 20:55:57 -0300

    Create MVC Archt/ and Dto

---
+++ backend/src/dto/patient/create.patient.dto.ts
@@ -0,0 +1,12 @@
+export type CreatePatientDto = {
+  id: string; 
+  name: string;      
+  cpf: string;
+  birthDate: Date|string;
+  phone: string;
+  email: string;
+  address: string;
+  city: string;
+  state: string;
+  notes: string;
+}
